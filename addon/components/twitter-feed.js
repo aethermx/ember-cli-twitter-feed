1850d43ca1dfe3d6835601199d4bf4b351f8c18f
Gastón I. Silva <givanse@gmail.com>
2015-09-05 00:28:11 -0500

    reload twitter widget js after component re-render (route transition)

--- addon/components/twitter-feed.js
+++ addon/components/twitter-feed.js
@@ -7,9 +7,21 @@ export default Ember.Component.extend({
   classNames: ['twitter_feed'],
 
   didInsertElement: function() {
+    let elementId = this.get('elementId');
+    Ember.assert('The first children must be a.twitter-timeline',
+                 Ember.$(elementId + ' > a.twitter-timeline'));
+
+    // TODO: instead of twitterWJS.remove() do twitterWJS.reEvaluate() ???
+
     /* jshint ignore:start */
     !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");
     /* jshint ignore:end */
-  }
+  },
+
+  willClearRender: function() {
+    let id = 'twitter-wjs';
+    let twitterWJS = document.getElementById(id);
+    twitterWJS.remove();
+  } 
 
 });
--- tests/integration/components/twitter-feed-test.js
+++ tests/integration/components/twitter-feed-test.js
@@ -6,12 +6,17 @@ moduleForComponent('twitter-feed', 'Integration | Component | twitter feed', {
 });
 
 test('it renders', function(assert) {
-  assert.expect(1);
+  assert.expect(2);
 
   // Set any properties with this.set('myProperty', 'value');
   // Handle any actions with this.on('myAction', function(val) { ... });
 
   this.render(hbs`{{twitter-feed}}`);
 
+  assert.equal(this.$().text().trim(), '');
+
+  // Block
+
+  this.render(hbs`{{#twitter-feed}}#emberjs Tweets{{/twitter-feed}}`);
   assert.equal(this.$().text().trim(), '#emberjs Tweets');
 });
